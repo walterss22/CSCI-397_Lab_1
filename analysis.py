a72c6f8d072e7140b0f021c7c1d2ff3698be1662
walterss22 <56270150+walterss22@users.noreply.github.com>
2021-10-15 23:20:09 -0400

    Complete task 2

--- analysis.py
+++ analysis.py
@@ -12,7 +12,7 @@ def countChars(txt):
     count = {}
     tot = 0
     for ndx in txt:
-        if ndx != " ":
+        if ndx != " " and ndx != "," and ndx != ".":
             if ndx not in count.keys():
                 count[ndx] = 1
             else:
@@ -25,36 +25,89 @@ def main():
     enc = open("encrypted.txt", "r").readline()
 
     count, tot = countChars(enc)
-
-    #print ("Raw count" + str(count))
-    print ("Total " + str(tot))
-
     perc = {}
 
     for key in count.keys():
-        perc[key] = round((count.get(key) / tot) * 100, 2)
+        perc[key] = (count.get(key) / tot) * 100
 
-    print(perc, "\n")
+    #print(perc, "\n")
 
-    #frequency table of English language
-    #sourced from Cornell University
-    #http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
+    #Letter frequency (for texts) table of English language
+    #sourced from Wikipedia
+    # https://en.wikipedia.org/wiki/Letter_frequency
     
-    freq = {"e" : 12.02, "t": 9.10, "a" : 8.12, "o" : 7.68, "i" : 7.31, "n" : 6.95, "s" : 6.28, "r" : 6.02, "h" : 5.92, "d" : 4.32, "l" : 3.98, "u" : 2.88, "c" : 2.71, "m" : 2.61, "f" : 2.30, "y" : 2.11, "w" : 2.09, "g" : 2.03, "p" : 1.82, "b" : 1.49, "v" : 1.11, "k" : 0.69, "x" : 0.17, "q" : 0.11, "j" : .10, "z" : 0.07}
+    freq = {"e" : 13, "t": 9.10, "a" : 8.2, "o" : 7.5, "i" : 7, "n" : 6.7, "s" : 6.3, "h" : 6.1, "r" : 6, "d" : 4.3, "l" : 4, "c" : 2.8, "u" : 2.8, "m" : 2.4, "w" : 2.4, "f" : 2.2, "g" : 2, "y" : 2, "p" : 1.9, "b" : 1.5, "v" : 0.98, "k" : 0.77, "j" : 0.15, "x" : 0.15, "q" : .095, "z" : 0.074}
     
     #KEY IS ENCRYPTED, VALUE IS DECRYPTED
     table = {}
 
     #match calculated keys to frequency table
-    """ for key in perc.keys():
-        for ndx in freq.keys():
-            if perc.get(key) == freq.get(ndx):
-                table[key] = ndx """
+    curr = list(perc.keys())[0]
+    last = ""
+    buff = 0
+    
+    while len(table.keys()) < 25:
+        for key in perc.keys():
+            if key in table:
+                continue
+            elif perc.get(key) > perc.get(curr):
+                if last == "" or perc.get(key) < perc.get(last) or curr == last:
+                    curr = key
+            #print("key: " + key)
+            #print("curr: " + curr)
+            #print("last: " + last)
+        
+        #print("End inner loop, " + str(x))
+        #print("add to table", "\n")
+        
 
-    for ndx in (0, len(perc.keys())):
-        table[perc.keys()[ndx]] = freq.keys()[ndx]
+        #this buffer keeps the current letter from being too high frequency
+        if not curr in table.keys():
+            table[curr] = list(freq.keys())[len(table)]
+        else:
+            buff +=1
+        last = curr
+        curr = list(perc.keys())[0 + buff]
 
-    print(table, "\n")
+    #print("len table: " + str(len(table)), "\n")
+    #print("Table: " + str(table))
+    #print("Percs: " + str(perc))
+
+    #print(table, "\n")
+    #print(len(table))
+    
+    #manual replacements
+    table['d'] = 'i'
+    table['z'] = 'o'
+    table['u'] = 'd'
+    #table['f'] = 'r'
+    table['k'] = 'y'
+    #table['t'] = 'w'
+    #table['y'] = 'p'
+    table['i'] = 'g'
+    table['c'] = 't'
+    table['n'] = 'e'
+    table['f'] = 's'
+    #table['v'] = 'r'
+    table['t'] = 'f'
+    table['x'] = 'w'
+    table['r'] = 'm'
+    #table['g'] = 'u'
+    table['w'] = 'v'
+    table['e'] = 'k'
+    table['j'] = 'l'
+    #table['l'] = 'h'
+    table['v'] = 'h'
+    #table['l'] = 'r'
+    table['g'] = 'b'
+    #table['h'] = 'u'
+    #table['l'] = 'p'
+    table['y'] = 'r'
+    table['h'] = 'p'
+    table['l'] = 'u'
+    table['p'] = 'q'
+    table['m'] = 'x'
+    table['b'] = 'z'
 
     #decrypt
     plain = ""
@@ -64,10 +117,12 @@ def main():
         else:
             plain += ndx
 
-    print(len(enc))
-    print(perc.keys(), "\n")
+    #print(len(enc))
+    #print(perc.keys(), "\n")
 
-    print(plain,"\n")
+    #print(plain,"\n")
+    plaintxt = open("plain.txt", 'x')
+    plaintxt.write(plain + "\nkey: " + str(table))
 
 if __name__ == "__main__":
     main()
